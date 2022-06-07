ae53cb76c78463c3ef578ccb82ff044f9a234e0d
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-06-07 14:04:51 +0200

    update checkbox.js

--- static/js/checkboxChecker.js
+++ static/js/checkboxChecker.js
@@ -1,34 +1,34 @@
 // if (window.location.href.indexOf("timelist") > -1) {
-  document.getElementById('footer-button-1').classList.add('active')
-  document.getElementById('footer-button-2').classList.remove('active')
-  const checkBoxLimit = () => {
-    var checkBoxGroup = document.forms['checkTime']['time'];			
-    var limit = 2;
-    for (var i = 0; i < checkBoxGroup.length; i++) {
-      checkBoxGroup[i].onclick = function() {
-        var checkedcount = 0;
-        for (var i = 0; i < checkBoxGroup.length; i++) {
-          checkedcount += (checkBoxGroup[i].checked) ? 1 : 0;
-        }
-        if (checkedcount > limit) {
-          console.log("You can select maximum of " + limit + " checkboxes.");
-          alert("You can select maximum of " + limit + " checkboxes.");						
-          this.checked = false;
-        }
+document.getElementById('footer-button-1').classList.add('active')
+document.getElementById('footer-button-2').classList.remove('active')
+const checkBoxLimit = () => {
+  var checkBoxGroup = document.forms['checkTime']['time'];
+  var limit = 2;
+  for (var i = 0; i < checkBoxGroup.length; i++) {
+    checkBoxGroup[i].onclick = function () {
+      var checkedcount = 0;
+      for (var i = 0; i < checkBoxGroup.length; i++) {
+        checkedcount += (checkBoxGroup[i].checked) ? 1 : 0;
+      }
+      if (checkedcount > limit) {
+        console.log("You can select maximum of " + limit + " checkboxes.");
+        alert("You can select maximum of " + limit + " checkboxes.");
+        this.checked = false;
       }
     }
   }
+}
 
-  checkBoxLimit()
+checkBoxLimit()
 
-  let time = {}
+let time = {}
 
-  const addTimeDB = () => {
+const addTimeDB = () => {
 
-    const fulltime = document.querySelector('').value
+  const fulltime = document.querySelector('').value
 
-    time.push(fulltime)
-  }
+  time.push(fulltime)
+}
 // }
 
 if (window.location.href.indexOf("manage") > -1) {
@@ -42,5 +42,7 @@ function startTime() {
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes();
   console.log(time);
-  setTimeout(function() {startTime()}, 10000);
+  setTimeout(function () {
+    startTime()
+  }, 10000);
 }
