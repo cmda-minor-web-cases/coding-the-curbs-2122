34f9c75c53c12b4cae6323773a8fca2d60ecbea4
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-06-09 15:22:28 +0200

    Pop up notification fixed

--- static/js/checkboxChecker.js
+++ static/js/checkboxChecker.js
@@ -3,8 +3,9 @@ document.getElementById('footer-button-1').classList.add('active')
 document.getElementById('footer-button-2').classList.remove('active')
 
 const checkBoxLimit = () => {
-  var checkBoxGroup = document.forms['checkTime']['time'];
+  var checkBoxGroup = document.forms['checkTime']['time']
   var checkBoxButton = document.getElementById("btn")
+  var maxAantal = document.getElementById("maxAantal")
   checkBoxButton.disabled = true;
   var limit = 2;
   for (var i = 0; i < checkBoxGroup.length; i++) {
@@ -15,11 +16,14 @@ const checkBoxLimit = () => {
       }
       if (checkedcount > limit) {
         console.log("You can select maximum of " + limit + " checkboxes.");
-        alert("You can select maximum of " + limit + " checkboxes.");
+        maxAantal.innerHTML = "Je kan maar maximaal 2 tijden selecteren";
         this.checked = false;
+        maxAantal.style.color = 'red';
       }
       if (checkedcount >= 1) {
         checkBoxButton.disabled = false;
+      } else {
+        checkBoxButton.disabled = true;
       }
     }
   }
