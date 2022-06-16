e09146b25eef13577c533e037afb0114ace28fae
maggness <timderoller@gmail.com>
2022-06-16 10:42:02 +0200

    changed map

--- static/js/mapCode.js
+++ static/js/mapCode.js
@@ -1,35 +1,69 @@
-const testbuttonselector = document.getElementById('testbutton')
-let colormarker = 'green'
+const testbuttonselector = document.getElementById("testbutton");
+let colormarker = document.getElementsByClassName("mapboxgl-marker");
 
-	// TO MAKE THE MAP APPEAR YOU MUST
-		// ADD YOUR ACCESS TOKEN FROM
-		// https://account.mapbox.com
-		mapboxgl.accessToken = 'pk.eyJ1IjoidG9tdmRiMTEiLCJhIjoiY2w0NWliMHBpMDEwODNjcGJ3aDc3OGYzeSJ9.x8AbcVT7uoUfCbjuVZq--w';
-	const map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/navigation-night-v1',
-	center: [5.123448523735618,52.082773415719515],
-	zoom: 12.5
-	});
-	
-	// // Create a default Marker and add it to the map.
-	// const marker1 = new mapboxgl.Marker()
-	// .setLngLat([5.133306556029083,52.08566089364163])
-	// .addTo(map);
-const updateMap = () => {
-	// Create a default Marker, colored black, rotated 45 degrees.
-	const marker2 = new mapboxgl.Marker({ color: colormarker, rotation: 10 })
-	.setLngLat([5.123448523735618,52.082773415719515])
-	.addTo(map);
+console.log(colormarker);
+
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidG9tdmRiMTEiLCJhIjoiY2w0NWliMHBpMDEwODNjcGJ3aDc3OGYzeSJ9.x8AbcVT7uoUfCbjuVZq--w";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/navigation-night-v1",
+  center: [5.123448523735618, 52.082773415719515],
+  zoom: 12.5,
+  pitch: 36,
+  hash: true,
+});
+
+// https://bl.ocks.org/chriswhong/8977c0d4e869e9eaf06b4e9fda80f3ab used for marker code
+
+class ClickableMarker extends mapboxgl.Marker {
+  // new method onClick, sets _handleClick to a function you pass in
+  onClick(handleClick) {
+    this._handleClick = handleClick;
+    return this;
+  }
+
+  // the existing _onMapClick was there to trigger a popup
+  // but we are hijacking it to run a function we define
+  _onMapClick(e) {
+    const targetElement = e.originalEvent.target;
+    const element = this._element;
+
+    if (
+      this._handleClick &&
+      (targetElement === element || element.contains(targetElement))
+    ) {
+      this._handleClick();
+    }
+  }
 }
 
-updateMap()
+// All palen
+const palen = [
+  {
+    name: "Utrecht",
+    coordinates: [5.123448523735618, 52.082773415719515],
+  },
+];
+
+palen.forEach((paal) => {
+  new ClickableMarker()
+    .setLngLat(paal.coordinates)
+    .onClick(() => {
+      // onClick() is a thing now!
+      console.log(paal.name);
+    })
+    .addTo(map);
+});
 
 testbuttonselector.addEventListener("click", function (e) {
-	if (colormarker === 'green') {
-		colormarker = 'red'
-	} else {
-		colormarker = 'green'
-	}
-	updateMap()
+  if (colormarker.getPropertyValue('--colorMarker') === "#56b956") {
+    colormarker.setProperty('--colorMarker', 'red');
+  } else {
+    colormarker.setProperty('--colorMarker', '#56b956');
+  }
+  // updateMap();
 });
